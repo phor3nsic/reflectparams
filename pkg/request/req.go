4674c7d0d04da112ef9bfd72c3061c98087a43db
Walleson Moura <phor3nsic@MacBook-Air-de-Walleson.local>
2022-04-27 09:59:28 -0300

    remove bugs of redirect

--- pkg/request/req.go
+++ pkg/request/req.go
@@ -1,27 +1,49 @@
 package request
 
 import (
+	"crypto/tls"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
 	"strings"
+	"time"
 )
 
+func noRedirect(req *http.Request, via []*http.Request) error {
+	return errors.New("Don't redirect!")
+}
+
 func Req(url string, param string) {
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Fatal(err)
+
+	var transport = &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: time.Second,
+			DualStack: true,
+		}).DialContext,
 	}
 
+	client := &http.Client{
+		Transport: transport,
+	}
+
+	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return nil
+	}
+
+	resp, err := client.Get(url)
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-	out := fmt.Sprintf("{\"name\":\"Reflect Params\",\"severity\":\"info\",\"url\":\"%s\"}", url)
+	//out := fmt.Sprintf("{\"name\":\"Reflect Params\",\"severity\":\"info\",\"url\":\"%s\"}", url)
 	sb := string(body)
 	if strings.Contains(sb, param) {
-		fmt.Println(out)
+		fmt.Println(url)
 	}
 
 }
