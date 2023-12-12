94b2ad9bbd445cf48c0957ca42f9de589223474d
phor3nsic <phor3nsic@pm.me>
2023-12-12 17:15:06 +0000

    more fast

--- pkg/read/file.go
+++ pkg/read/file.go
@@ -4,11 +4,13 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"sync"
 
 	"github.com/phor3nsic/reflectparams/pkg/request"
 )
 
 func ReadFile(path string, param string) {
+	var wg sync.WaitGroup
 
 	var line string
 
@@ -23,9 +25,15 @@ func ReadFile(path string, param string) {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line = scanner.Text()
-		request.Req(line, param)
+		wg.Add(1)
+		go func(url string) {
+			defer wg.Done()
+			request.Req(url, param)
+		}(line)
 
 	}
+
+	wg.Wait()
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
@@ -34,11 +42,18 @@ func ReadFile(path string, param string) {
 }
 
 func ReadStdin(param string) {
+	var wg sync.WaitGroup
 	var line string
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		line = scanner.Text()
-		request.Req(line, param)
+		wg.Add(1)
+		go func(url string) {
+			defer wg.Done()
+			request.Req(url, param)
+		}(line)
 	}
+
+	wg.Wait()
 
 }
