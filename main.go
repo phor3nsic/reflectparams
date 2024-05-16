f62eac48ce822aa5ac475388a5fdd07a7402f1fe
phor3nsic <phorensic@protonmail.com>
2024-05-16 18:44:16 +0100

    add concorrence parameter

--- main.go
+++ main.go
@@ -5,15 +5,16 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/phor3nsic/reflectparams/pkg/globalvar"
+	"github.com/phor3nsic/reflectparams/pkg/globalvar" //Use the file with global variables
 	"github.com/phor3nsic/reflectparams/pkg/read"
 )
 
 func main() {
-	flag.StringVar(&globalvar.Parameter, "p", "", "Param to check")
-	flag.StringVar(&globalvar.ListUrls, "l", "", "List Of Urls")
-	flag.StringVar(&globalvar.Proxy, "x", "", "Replay proxy requests")
-	flag.StringVar(&globalvar.Inject, "i", "", "Inject special chars on parameter...")
+	flag.StringVar(&globalvar.Parameter, "p", "", "Param to check. Ex: FUZZ")
+	flag.StringVar(&globalvar.ListUrls, "l", "", "List of urls. Ex: /tmp/urls.txt")
+	flag.StringVar(&globalvar.Proxy, "x", "", "Replay proxy requests. Ex: http://localhost:8080")
+	flag.StringVar(&globalvar.Inject, "i", "", "Inject special chars on parameter. Ex: ['\"><s>s]")
+	flag.IntVar(&globalvar.Concorrency, "c", 10, "Concorrency value.")
 	flag.Parse()
 
 	if globalvar.Parameter == "" {
@@ -22,9 +23,9 @@ func main() {
 	}
 
 	if globalvar.ListUrls != "" {
-		read.ReadFile(globalvar.ListUrls)
+		read.ReadFile(globalvar.ListUrls) //If with list file
 	} else {
-		read.ReadStdin()
+		read.ReadStdin() //If by stdin
 	}
 
 }
