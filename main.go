f3c85787074348a012b3e142dd88b0b95d1ef01f
phor3nsic <phorensic@protonmail.com>
2024-05-17 11:35:29 +0100

    Adjust concorrency

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	flag.StringVar(&globalvar.ListUrls, "l", "", "List of urls. Ex: /tmp/urls.txt")
 	flag.StringVar(&globalvar.Proxy, "x", "", "Replay proxy requests. Ex: http://localhost:8080")
 	flag.StringVar(&globalvar.Inject, "i", "", "Inject special chars on parameter. Ex: ['\"><s>s]")
-	flag.IntVar(&globalvar.Concorrency, "c", 10, "Concorrency value.")
+	flag.IntVar(&globalvar.Concorrency, "c", 5, "Concorrency value.")
 	flag.Parse()
 
 	if globalvar.Parameter == "" {
